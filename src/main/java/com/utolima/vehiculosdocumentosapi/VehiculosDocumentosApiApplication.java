444420538a63efb6589656ac806142f921ee9c91
Nico <inge.nicoespi@gmail.com>
2026-09-10 00:43:43 -0500

    Proyecto base Spring Boot con dependencias iniciales

---
+++ src/main/java/com/utolima/vehiculosdocumentosapi/VehiculosDocumentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.utolima.vehiculosdocumentosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosDocumentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosDocumentosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utolima/vehiculosdocumentosapi/VehiculosDocumentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utolima.vehiculosdocumentosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosDocumentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
